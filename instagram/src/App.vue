8e7af3d1d57386d8efcfcd75a0e24ef5ebe922f6
HarshitaKillana <83214994+HarshitaKillana@users.noreply.github.com>
2023-12-10 12:36:05 +0530

    userProfileAdd

--- instagram/src/App.vue
+++ instagram/src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <RegistryPage></RegistryPage>
   </nav>
-  <router-view/>
 </template>
+
+
+<script>
+import RegistryPage from './components/RegistryPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RegistryPage
+}
+}
+
+</script>
+
+
 
 <style>
 #app {
